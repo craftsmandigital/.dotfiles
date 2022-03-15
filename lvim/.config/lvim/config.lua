bfba5b6815d63c8b28c2bf6fa6230044018dec35
craftsmandigital <hackjack@tutanota.com>
2022-03-15 14:59:44 +0100

    Added HTML suport to NJK files in vim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -195,6 +195,8 @@ lvim.plugins = {
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
+lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
+  { "BufReadPost", "*.njk", "set filetype=html" },
+  
+}
